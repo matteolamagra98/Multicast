e2e69965a581f75f3cdae7085fb9e8391800c59a
matteolamagra98 <83699057+matteolamagra98@users.noreply.github.com>
2021-10-22 19:49:14 +0200

    Add files via upload

---
+++ Receiver.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+cd C:\Sviluppo\AIRNav\MulticastReceiver\target
+java -jar MulticastReceiver-0.0.1-SNAPSHOT.jar 5000 224.4.5.6 2
+pause
---
+++ Sender.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+cd C:\Sviluppo\AIRNav\MulticastSender\target
+java -jar MulticastSender-0.0.1-SNAPSHOT.jar 5000 224.4.5.6 C:\Sviluppo\AIRNav\MulticastReceiver\Dati_2021_10_18_17_34.bin
+pause
